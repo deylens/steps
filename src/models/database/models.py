46f3d94a9107ace7f3ded1396365783f2f896e11
Vasily Oleinikov <vasia94.94@gmail.com>
2025-02-28 16:37:17 +0300

    configured alembic

--- src/models/database/models.py
+++ src/models/database/models.py
@@ -1,5 +1,4 @@
-
-from sqlalchemy import Integer,BigInteger, Column, Boolean,String,Text,Date,ForeignKey,create_engine, MetaData
+from sqlalchemy import Integer, BigInteger, Column, Boolean, String, Text, Date,ForeignKey, create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, relationship, DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -8,7 +7,7 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     telegram_id = Column(BigInteger, unique=True)
     children = relationship('Child', back_populates='user')
 
@@ -18,12 +17,11 @@ class User(Base):
 class Child(Base):
     __tablename__ = 'children'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     name = Column(String, nullable=False)
     birth_date = Column(Date, nullable=False)
-
-    user = relationship('User',back_populates='children')
+    user = relationship('User', back_populates='children')
     diagnosis_history = relationship("DiagnosisHistory", back_populates='child')
     diagnosis_result = relationship("DiagnosisResult", back_populates='child')
 
@@ -33,9 +31,8 @@ class Child(Base):
 class SkillType(Base):
     __tablename__ = 'skill_types'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, unique=True, index=True)
-
     skill = relationship("Skill", back_populates='skill_type')
     diagnosis_result = relationship("DiagnosisResult", back_populates='skill_type')
     diagnosis_history = relationship("DiagnosisHistory", back_populates='skill_type')
@@ -46,7 +43,7 @@ class SkillType(Base):
 class Skill(Base):
     __tablename__ = 'skills'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     skill_type_id = Column(Integer, ForeignKey('skill_types.id'), nullable=False)
     name = Column(String, nullable=False)
     criteria = Column(Text)
@@ -54,12 +51,9 @@ class Skill(Base):
     age_start = Column(Integer)
     age_end = Column(Integer)
     actual_age = Column(Integer)
-
     skill_type = relationship("SkillType", back_populates='skill')
     diagnosis_history = relationship("DiagnosisHistory", back_populates='skill')
    
-
-
     def __repr__(self):
         return f"{self.name}"
 
@@ -83,30 +77,13 @@ class DiagnosisHistory(Base):
 class DiagnosisResult(Base):
     __tablename__ = 'diagnosis_result'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     child_id = Column(Integer, ForeignKey('children.id'), nullable=False)
     date = Column(Date)
     skill_types_id = Column(Integer, ForeignKey("skill_types.id"), nullable=False)
     age_assessment = Column(Integer)
-
     skill_type = relationship("SkillType", back_populates='diagnosis_result')
     child = relationship('Child', back_populates='diagnosis_result')
 
-
     def __repr__(self):
         return f"{self.child_id} {self.skill_type_id} {self.age_assessment}"
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    print(Base.metadata)
-    Base.metadata.create_all(engine)
-    print(Base.metadata)
-    Base.metadata.reflect(engine)
-    Base.metadata.drop_all(bind=engine)
-    print(Base.metadata)
-    # Base.metadata.create_all(engine)
